03c97f3cea6f5e3d369e172044752e2a9c43dad6
sandro-ye <ye.sandro2003@gmail.com>
2026-01-05 15:42:38 +0100

    modificato FileRepositoryPersistence.java

--- src/main/java/it/unibs/visite/persistence/FileRepositoryPersistence.java
+++ src/main/java/it/unibs/visite/persistence/FileRepositoryPersistence.java
@@ -1,22 +1,30 @@
 package it.unibs.visite.persistence;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.NoSuchFileException;
+import java.nio.file.Path;
+import java.util.function.Supplier;
 
 public class FileRepositoryPersistence {
 
-    public static <T> T caricaOggetto(String filename, Class<T> clazz) {
+    public static <T extends Serializable> T caricaOggetto(Path filename, Supplier<T> defaultSupplier) {
         // Implementazione del caricamento da file
-        try(ObjectInputStream ois = new ObjectInputStream(new FileInputStream(filename))) {
+        try(ObjectInputStream ois = new ObjectInputStream(Files.newInputStream(filename))) {
             Object obj = ois.readObject();
-            return clazz.cast(obj);
+            return (T) obj;
+        } catch(NoSuchFileException e) { 
+            T defaultObject = defaultSupplier.get();
+            salvaOggetto(defaultObject, filename);
+            return defaultObject;
         } catch (Exception e) {
             throw new RuntimeException("errore durante caricamento oggetto da file: " + filename, e);
         }
     }
 
-    public static void salvaOggetto(Object repository, String filename) {
+    public static void salvaOggetto(Object repository, Path filename) {
         // Implementazione del salvataggio su file
-        try(ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(filename))) {
+        try(ObjectOutputStream oos = new ObjectOutputStream(Files.newOutputStream(filename))) {
             oos.writeObject(repository);
         } catch (Exception e) {
             throw new RuntimeException("errore durante salvataggio oggetto su file: " + filename, e);
